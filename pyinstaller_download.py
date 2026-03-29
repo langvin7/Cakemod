890e9ae896d336c3e3afb622029585f6a4dd47fe
cake2233 <15084858+cake2233@user.noreply.gitee.com>
2026-03-29 10:32:12 +0800

    移除Python脚本和mods.zip

--- pyinstaller_download.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import zipfile
-print('Downloading...')
-urllib.request.urlretrieve('https://github.com/pyinstaller/pyinstaller/archive/refs/tags/v6.5.0.zip', 'pyi.zip')
-print('Extracting...')
-zipfile.ZipFile('pyi.zip').extractall()
-print('Done!')
